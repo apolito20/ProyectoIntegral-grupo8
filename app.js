eff7f04a655b65ec34ec89436a0b7765e433c404
JuliettAsan <juliethsanchez9595@gmail.com>
2021-11-15 13:11:12 -0500

    agrego estructura de archivos con node.js+ express y index.html

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.listen(3300, () => {
+    console.log("Servidor funcionando");
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
