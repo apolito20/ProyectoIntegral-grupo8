30c4acdaeb455c51a6beda3e98f0c8eeb8530350
Andrea Polito <andreaepolito@hotmail.com>
2021-11-17 18:18:45 -0300

    Agrego links de app.js de login y register Corregi el lazo con el css correcto

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ app.get("/", (req,res) => {
 app.get('/detalleProducto', (req,res)=>{
     res.sendFile(__dirname + '/views/detalleProducto.html');
 });
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
